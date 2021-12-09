76338989ce438e3282fa19cf48312b2ec8729064
Timothy D Meadows II <timothy@timothymeadows.com>
2021-12-08 19:56:17 -0800

    Add working camera example.

--- Yolo5.NetCore.Examples/Program.cs
+++ Yolo5.NetCore.Examples/Program.cs
@@ -9,25 +9,10 @@ namespace Yolo5.NetCore.Examples
     {
         static void Main(string[] args)
         {
-            using var image = Image.FromFile("input.jpg");
-            using var yolo = new Yolo<YoloCocoModel>("Models/yolov5n6.onnx");
-            var predictions = yolo.Predict(image);
+            // Single frame / image example.
+            // ImageExample.Run();
 
-            using var graphics = Graphics.FromImage(image);
-            foreach (var prediction in predictions) // iterate predictions to draw results
-            {
-                var score = Math.Round(prediction.Score, 2);
-                graphics.DrawRectangles(new Pen(Color.Blue, 1),
-                    new[] { prediction.Rectangle });
-
-                var (x, y) = (prediction.Rectangle.X - 3, prediction.Rectangle.Y - 23);
-
-                graphics.DrawString($"{prediction.Label.Name} ({score})",
-                    new Font("Consolas", 16, GraphicsUnit.Pixel), new SolidBrush(Color.White),
-                    new PointF(x, y));
-            }
-
-            image.Save("output.jpg");
+            CameraExample.Run();
         }
     }
 }
